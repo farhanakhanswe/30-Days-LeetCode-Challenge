21005621cdbc2e4dc6e8a37228bd8571fef3ef07
Farhana Khan <134283374+farhanakhanswe@users.noreply.github.com>
2023-07-25 23:52:52 +0600

    correct soln update for find-pivot-index question

--- Problem Solving/Arrays-And-Hashing/find-pivot-index.js
+++ Problem Solving/Arrays-And-Hashing/find-pivot-index.js
@@ -1,29 +1,25 @@
 /* Question Link: https://leetcode.com/problems/find-pivot-index/ */
 
-// NOT CORRECT //
-
 var pivotIndex = function(nums) {
-    
     let leftSum = 0;
     let rightSum = 0;
-    let leftMostPivotIndex = 0;
+    let leftMostPivotIndex = -1;
 
-    for(let i = 0; i < nums.length; i++)
-    {
-        leftSum+= nums[i];
-        rightSum -= nums[nums.length - 1 - i];
+    // Calculate the sum of all elements in the array (rightSum)
+    for (let num of nums) {
+        rightSum += num;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        rightSum -= nums[i];
         
-        if(leftSum === rightSum)
-        {
-            leftMostPivotIndex =  i;
+        if (leftSum === rightSum) {
+            leftMostPivotIndex = i;
             break;
         }
+
+        leftSum += nums[i];
     }
 
-    if(leftMostPivotIndex != 0)
-    {
-        return leftMostPivotIndex;
-    }
-    
-    return -1;
-}
+    return leftMostPivotIndex;
+};
