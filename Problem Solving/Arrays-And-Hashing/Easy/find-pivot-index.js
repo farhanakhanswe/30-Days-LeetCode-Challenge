6b29c059e65ab9fa38fa5fe8d7830644d7217bb4
farhanakhanswe <farhanakhanswe@gmail.com>
2023-10-28 00:22:14 +0600

    brute force soln added for find-pivot-index question

--- Problem Solving/Arrays-And-Hashing/Easy/find-pivot-index.js
+++ Problem Solving/Arrays-And-Hashing/Easy/find-pivot-index.js
@@ -1,25 +1,36 @@
 /* Question Link: https://leetcode.com/problems/find-pivot-index/ */
 
-var pivotIndex = function(nums) {
+/* Brute Force Solution 
+    Time Complexity: O(n^2)
+    Space Complexity: O(1)
+*/
+
+const pivotIndex = (nums) => {
+
+    if (nums.length === 1) {
+        return 0;
+    }
+
     let leftSum = 0;
     let rightSum = 0;
-    let leftMostPivotIndex = -1;
-
-    // Calculate the sum of all elements in the array (rightSum)
-    for (let num of nums) {
-        rightSum += num;
-    }
 
     for (let i = 0; i < nums.length; i++) {
-        rightSum -= nums[i];
-        
-        if (leftSum === rightSum) {
-            leftMostPivotIndex = i;
-            break;
+        for (let j = 0; j < i; j++) // i = 2 j = 0 + 1
+        {
+            leftSum += nums[j];
         }
 
-        leftSum += nums[i];
+        for (let k = nums.length - 1; k > i; k--) {
+            rightSum += nums[k];
+        }
+
+        if (leftSum == rightSum) return i;
+
+        leftSum = 0;
+        rightSum = 0;
+
     }
 
-    return leftMostPivotIndex;
+    return -1;
 };
+
