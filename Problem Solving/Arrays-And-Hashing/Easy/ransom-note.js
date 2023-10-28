368d8f57a49b0131a6894aac7f1743bf639a8269
farhanakhanswe <farhanakhanswe@gmail.com>
2023-10-29 00:39:51 +0600

    solution update on ransom note

--- Problem Solving/Arrays-And-Hashing/Easy/ransom-note.js
+++ Problem Solving/Arrays-And-Hashing/Easy/ransom-note.js
@@ -1,23 +1,41 @@
 /* Question Link: https://leetcode.com/problems/ransom-note/ */
 
-// NOT accepted on leetcode
+/* Optimized Solution : 
+   Time Complexity -> O(n) and
+   Space Complexity -> O(n)
+*/
 
-var canConstruct = (ransomNote, magazine) => {
-    
-    let magazineSet = new Set(magazine);
-    
-    for( let i = 0 ; i < ransomNote.length ; i++)
-    {
-        currEl = ransomNote[i];
-        
-        if(!magazineSet.has(currEl))
-        {
-            return false; 
+const canConstruct = (ransomNote, magazine) => {
+    let ransomNoteMap = new Map();
+    let magazineMap = new Map();
+
+    for (let i = 0; i < ransomNote.length; i++) {
+        let char = ransomNote[i].toLowerCase();
+
+        if (ransomNoteMap.has(char)) {
+            let currCount = ransomNoteMap.get(char);
+            ransomNoteMap.set(char, currCount + 1);
+        } else {
+            ransomNoteMap.set(char, 1);
         }
-        
-        magazineSet.delete(currEl);
-        
     }
+
+    for (let i = 0; i < magazine.length; i++) {
+        let char = magazine[i].toLowerCase();
+
+        if (magazineMap.has(char)) {
+            let currCount = magazineMap.get(char);
+            magazineMap.set(char, currCount + 1);
+        } else {
+            magazineMap.set(char, 1);
+        }
+    }
+
+    for (let [char, count] of ransomNoteMap.entries()) {
+        if (!magazineMap.has(char) || magazineMap.get(char) < count) {
+            return false;
+        }
+    }
+
     return true;
-    
 }
