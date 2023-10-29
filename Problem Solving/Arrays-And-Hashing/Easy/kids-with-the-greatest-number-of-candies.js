d67fc1d80fe6a1c7a528bbb218e3d234a1c7cd28
farhanakhanswe <farhanakhanswe@gmail.com>
2023-10-29 14:07:01 +0600

    soln update for kids with the greatest number of candies

--- Problem Solving/Arrays-And-Hashing/Easy/kids-with-the-greatest-number-of-candies.js
+++ Problem Solving/Arrays-And-Hashing/Easy/kids-with-the-greatest-number-of-candies.js
@@ -5,31 +5,22 @@ Time Complexity : 0(n)
 Space Complexity: 0(n)
 */
 
-var kidsWithCandies = function(candies, extraCandies)
-{
-    
-    let childCandiesMap = new Map();
+const kidsWithCandies = (candies, extraCandies) => {
+    let largestNum = 0;
+    let totalCandies = 0;
     let result = [];
-    
-    for( let i = 0 ; i < candies.length ; i++ )
-    {
-        childCandiesMap.set(i, candies[i],);
-    }    
 
-    for( let n = 0 ; n < candies.length ; n++)
-    {
-        numOfCandiesOfNthKid = childCandiesMap.get(n);
-        
-        numOfCandiesOfNthKid += extraCandies;
-        
-        if( numOfCandiesOfNthKid >= Math.max(...childCandiesMap.values()))
-        {
-            result.push(true);
-        }else
-        {
-            result.push(false)
+    for (let i = 0; i < candies.length; i++) {
+        if (candies[i] > largestNum) {
+            largestNum = candies[i];
         }
     }
-    
+
+    for (let i = 0; i < candies.length; i++) {
+        totalCandies = candies[i] + extraCandies;
+
+        totalCandies >= largestNum ? result.push(true) : result.push(false);
+    }
+
     return result;
 }
